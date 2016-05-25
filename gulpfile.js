15578495e88386f607ebde60a817270d44d6d8ed
Faouzi OUDOUH <faouzi_far@hotmail.fr>
2016-05-25 23:31:28 +0100

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+
+var babel = require('gulp-babel');
+var gulp = require('gulp');
+var print = require('gulp-print');
+var webserver = require('gulp-webserver');  
+
+
+gulp.task('js', function() {
+  return gulp.src('app/js/*.js')               
+        .pipe(print())                         
+      .pipe(babel({ presets: ['es2015'] }))    
+      .pipe(gulp.dest('build/js'));             
+});
+
+gulp.task('libs', function(){
+    return gulp.src([
+        'node_modules/systemjs/dist/system.js',
+        'node_modules/babel-polyfill/dist/polyfill.js'])
+        .pipe(print())
+        .pipe(gulp.dest('build/libs'));
+});
+
+gulp.task('build', function(){
+    return gulp.src('app/**/*.*')
+            .pipe(print())
+            .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['js', 'libs'], function(){
+    return gulp.src(['app/**/*.html', 'app/**/*.css'])
+            .pipe(print())
+            .pipe(gulp.dest('build'));
+});
+
+gulp.task('serve', ['build'], function() {
+    gulp.src('build')
+        .pipe(webserver({open: true}));
+});
